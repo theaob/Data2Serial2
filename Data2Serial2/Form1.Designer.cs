416e8a3f2b5927e5f53f363f2c71ec636f0f2d1b
Onur <theaob@gmail.com>
2012-06-21 10:38:19 +0300

    initial version

---
+++ Data2Serial2/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Data2Serial2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TabPage tabPage1;
+            System.Windows.Forms.TabPage tabPage5;
+            System.Windows.Forms.TabPage tabPage4;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            tabPage1 = new System.Windows.Forms.TabPage();
+            tabPage5 = new System.Windows.Forms.TabPage();
+            tabPage4 = new System.Windows.Forms.TabPage();
+            tabPage1.SuspendLayout();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(this.splitContainer2);
+            tabPage1.Location = new System.Drawing.Point(4, 22);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Size = new System.Drawing.Size(345, 137);
+            tabPage1.TabIndex = 3;
+            tabPage1.Text = "Send";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer2.Size = new System.Drawing.Size(345, 137);
+            this.splitContainer2.SplitterDistance = 171;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 137);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dump File to Port";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(170, 137);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // tabPage5
+            // 
+            tabPage5.Location = new System.Drawing.Point(4, 22);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Size = new System.Drawing.Size(345, 137);
+            tabPage5.TabIndex = 2;
+            tabPage5.Text = "Settings";
+            tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Location = new System.Drawing.Point(4, 22);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new System.Drawing.Size(345, 137);
+            tabPage4.TabIndex = 1;
+            tabPage4.Text = "COM Port";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(353, 399);
+            this.splitContainer1.SplitterDistance = 232;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.IntegralHeight = false;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(353, 232);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.UseTabStops = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(tabPage4);
+            this.tabControl1.Controls.Add(tabPage5);
+            this.tabControl1.Controls.Add(tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.ItemSize = new System.Drawing.Size(58, 18);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(353, 163);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text File (.txt)|*.txt|Log File (.log)|*.log|Other|*.*";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(116, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 399);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Data2Serial2";
+            tabPage1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+
+    }
+}
+
---
+++ Data2Serial2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+[assembly: CLSCompliant(true)]
+namespace Data2Serial2
+{
+
+    public partial class Form1 : Form
+    {
+
+        private SerialPort port = new SerialPort();
+
+        private LinkedList<String> linesRead = new LinkedList<string>();
+        private LinkedList<byte[]> hexLines = new LinkedList<byte[]>();
+        private String version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public Form1()
+        {
+            InitializeComponent();
+            changeTitle(version);
+            addToList("Application ("+version+") opened");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            linesRead.Clear();
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            
+            String filename = openFileDialog1.FileName;
+            if (filename.Length < 2)
+            {
+                return;
+            }
+
+            String shortName = filename.Substring(filename.LastIndexOf('\\') + 1);
+            addToFileList(shortName);
+            changeTitle(shortName);
+
+            try
+            {
+                TextReader tr = new StreamReader(filename);
+
+                String line = "";
+
+                while ((line = tr.ReadLine()) != null)
+                {
+                    linesRead.AddLast(line);
+                    hexLines.AddLast(StringToByteArray(line));
+                    addToList(line);
+                }
+
+                tr.Close();
+            }
+
+            catch (System.IO.IOException)
+            {
+                showError("Couldn't read selected file!");
+            }
+        }
+
+        private static byte[] StringToByteArray(string hex)
+        {
+            try
+            {
+                System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+                return encoding.GetBytes(hex);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                showError("Error in hex conversion!");
+            }
+            catch (ArgumentNullException)
+            {
+                showError("Error in hex conversion!");
+
+            }
+            catch (ArgumentException)
+            {
+                showError("Error in hex conversion!");
+            }
+            return null;
+        }
+
+        private static void showError(String message)
+        {
+            MessageBox.Show(message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, (MessageBoxOptions) 0);
+        }
+
+        private void changeTitle(String text)
+        {
+            this.Text = "Data2Serial2 - " + text;
+            //Form1.ActiveForm.Text 
+        }
+
+        private void addToList(String text)
+        {
+            //listBox1.Items.Add(DateTime.Now.ToString("HH:mm:ss:ff | " + text));
+            //Following is faster
+            listBox1.Items.Insert(listBox1.Items.Count, DateTime.Now.ToString("HH:mm:ss:ff",null) + " | " + text);
+            //listBox1.SetSelected(listBox1.Items.Count - 1, true);
+        }
+
+
+        private void addToFileList(String text)
+        {
+            comboBox1.Items.Insert(comboBox1.Items.Count, text);
+            comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+        }
+
+        //private void addToListSecure(String text)
+        //{
+        //}
+    }
+}
